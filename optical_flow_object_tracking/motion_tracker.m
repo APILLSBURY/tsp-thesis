3346b5f50381f6bf75e58febcdadf02db517a3fd
Andrew Pillsbury <andrew.d.pillsbury@gmail.com>
2015-05-21 09:52:36 -0400

    moved figures, added results, and added writeup

--- optical_flow_object_tracking/motion_tracker.m
+++ optical_flow_object_tracking/motion_tracker.m
@@ -1,11 +1,11 @@
-root = 'sequences/wheelock1/'; % folder holding flow_folder and image files
-flow_folder = 'flow/'; % name of the folder holding the optical flow files in the root directory
-input_video = 'video/wheelock1.mov'; % video to track motion of
-output_video = 'results/wheelock1.avi'; % name of video to create
+root = 'sequences/wheelock_small/'; % folder holding flow_folder and image files
+flow_folder = 'flow1/'; % name of the folder holding the optical flow files in the root directory
+input_video = 'video/wheelock_small.mov'; % video to track motion of
+output_video = 'results/wheelock_small1.avi'; % name of video to create
 img_suffix = 'jpg'; % type of images used
 
 params.sameness_threshold = 0.4; % the sameness amount necessary for two masks to be considered the same object
-params.frame_inc = 5; % interval of frames to look at
+params.frame_inc = 1; % interval of frames to look at
 params.expansion = 40; % the amount (radius) to expand each object mask by
 params.percentage_tolerance = 8; % tolerance for flow direction in radians
 params.min_object_percentage = 1; % the minimum percentage of the image an object can be for it to be taken into consideration
